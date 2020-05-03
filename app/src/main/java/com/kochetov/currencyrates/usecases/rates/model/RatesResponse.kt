e51db3379fcd3921bdb722b557ce62fbcc01952f
konstantin kochetov <kochetov0824@gmail.com>
2020-05-03 19:40:17 +0200

    format to us locale to have dots instead of commas like in germany

--- app/src/main/java/com/kochetov/currencyrates/usecases/rates/model/RatesResponse.kt
+++ app/src/main/java/com/kochetov/currencyrates/usecases/rates/model/RatesResponse.kt
@@ -29,7 +29,7 @@ fun RatesResponse.toRatesMap(base: Rate): Map<String, Rate> {
     rates.forEach {
         map[it.key] = Rate(
             currency = Currency.getInstance(it.key),
-            amount = "%.2f".format(it.value * base.amount).toDoubleOrNull() ?: 0.0,
+            amount = "%.2f".format(Locale.US, it.value * base.amount).toDoubleOrNull() ?: 0.0,
             imageResString = "$FLAG_PREFIX${it.key.toLowerCase(Locale.getDefault())}"
         )
     }
